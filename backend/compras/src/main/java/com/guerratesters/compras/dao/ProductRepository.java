73d35103d1e7819bd5cab99a6265589c8b62c2bc
Nicolás <nicolascaytuirosilva@gmail.com>
2022-04-29 11:54:10 -0500

    Se agrego la busqueda de productos por categorias

--- backend/compras/src/main/java/com/guerratesters/compras/dao/ProductRepository.java
+++ backend/compras/src/main/java/com/guerratesters/compras/dao/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.guerratesters.compras.dao;
 
 import com.guerratesters.compras.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
--- frontend/angular-compras/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-compras/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -12,14 +13,33 @@ import { Product } from '../../common/product';
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
-  constructor(private producService: ProductService) { }
+  currentCategoryId: number = 0;
+
+  constructor(private producService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
     this.listProducts();
   }
 
   listProducts() {
-    this.producService.getProductList().subscribe(
+
+    // Check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId) {
+      // Get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+    } else {
+      // Not category id available... default category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // Now get the products for the given category id
+    this.producService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
