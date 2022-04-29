b9dc0563a33a14d413db178c5e49c05a74ec9b34
Nicolás <nicolascaytuirosilva@gmail.com>
2022-04-29 13:45:31 -0500

    Se modifico la busqueda de productos por categoria (busqueda dinamica)

--- frontend/angular-compras/src/app/services/product.service.ts
+++ frontend/angular-compras/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators'
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { map } from 'rxjs/operators'
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products'
+  private categoryUrl = 'http://localhost:8080/api/product-category'
 
   constructor(private httpClient: HttpClient) {}
 
@@ -18,14 +20,26 @@ export class ProductService {
     // need to build URL based on the category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(reponse => reponse._embedded.products)
+    )
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(reponse => reponse._embedded.productCategory)
     )
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
